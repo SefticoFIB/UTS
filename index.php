479612ec922dc51c5087889ea48b633b5098dd41
PrinCZyS <hieronimus.tiko123@gmail.com>
2023-05-01 23:12:16 +0700

    New Update File index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "DB-redlock";
-$username = "dockerPhp";
+$username = "php_docker";
 $password = "password";
 $dbname = "redlock-db";
 
@@ -12,18 +12,13 @@ if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-// Menampilkan seluruh isi tabel users
-$sql = "SELECT * FROM users";
+$sql = "SELECT COUNT(*) as total FROM users";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    // Menampilkan data setiap baris dari tabel
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "ID: " . $row["ID"] . " - Nama: " . $row["Nama"] . " - Alamat: " . $row["Alamat"] . " - Jabatan: " . $row["Jabatan"] . "<br>";
-    }
+    // Menampilkan jumlah user
+    $row = mysqli_fetch_assoc($result);
+    echo "Jumlah user: " . $row["total"];
 } else {
     echo "0 results";
 }
-
-mysqli_close($conn);
-?>
